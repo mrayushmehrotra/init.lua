b07598246142f737e2d5076c66f16ad3cca4481e
Ayush Mehrotra <ayushmehrotra099@gmail.com>
2023-12-26 09:26:51 +0530

    added on GUI

--- lua/ayush/plugins/alpha-nvim.lua
+++ lua/ayush/plugins/alpha-nvim.lua
@@ -21,7 +21,8 @@ return {
 		-- Set menu
 		dashboard.section.buttons.val = {
 			dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-			dashboard.button("SPC e", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+			dashboard.button("SPC e", "  > Focus file explorer", "<cmd>NvimTreeFocus<CR>"),
+			dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
 			dashboard.button("SPC ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
 			dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
 			dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
