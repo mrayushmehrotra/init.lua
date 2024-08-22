17ea681731110578c3b8571ee4b2c21697ff7bbf
ayush <ayushmehrotra099@gmail.com>
2024-08-22 12:43:08 +0530

    added split window movements

--- lua/ayush/core/keymaps.lua
+++ lua/ayush/core/keymaps.lua
@@ -24,6 +24,12 @@ keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) 
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
+-- move between splits
+keymap.set("n", "<leader>wh", "<C-w>h", { desc = "Move to the left split" })
+keymap.set("n", "<leader>wj", "<C-w>j", { desc = "Move to the split below" })
+keymap.set("n", "<leader>wk", "<C-w>k", { desc = "Move to the split above" })
+keymap.set("n", "<leader>wl", "<C-w>l", { desc = "Move to the right split" })
+
 keymap.set("n", "<leader>tt", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 keymap.set("n", "<leader>tr", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
 keymap.set("n", "<tab>q", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
--- lua/ayush/plugins/telescope.lua
+++ lua/ayush/plugins/telescope.lua
@@ -12,17 +12,20 @@ return {
 		local telescope = require("telescope")
 		local actions = require("telescope.actions")
 		local transform_mod = require("telescope.actions.mt").transform_mod
-
-		local trouble = require("trouble")
 		local trouble_telescope = require("trouble.providers.telescope")
+		local keymap = vim.keymap -- for conciseness
+		local builtin = require("telescope.builtin")
 
-		-- or create your custom action
+		vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+		vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+		-- Custom action for opening trouble quickfix list
 		local custom_actions = transform_mod({
 			open_trouble_qflist = function(prompt_bufnr)
-				trouble.toggle("quickfix")
+				require("trouble").toggle("quickfix")
 			end,
 		})
 
+		-- Setup Telescope with custom mappings
 		telescope.setup({
 			defaults = {
 				path_display = { "smart" },
@@ -37,15 +40,28 @@ return {
 			},
 		})
 
+		-- Load the fzf extension
 		telescope.load_extension("fzf")
 
-		-- set keymaps
-		local keymap = vim.keymap -- for conciseness
-
+		-- Set keymaps for Telescope
 		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
 		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
 		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
 		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
 		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+
+		-- Set keymaps for todo-comments navigation
+		keymap.set("n", "]t", function()
+			require("todo-comments").jump_next()
+		end, { desc = "Next todo comment" })
+
+		keymap.set("n", "[t", function()
+			require("todo-comments").jump_prev()
+		end, { desc = "Previous todo comment" })
+
+		-- Set keymap for jumping to next error/warning todo comment
+		keymap.set("n", "]e", function()
+			require("todo-comments").jump_next({ keywords = { "ERROR", "WARNING" } })
+		end, { desc = "Next error/warning todo comment" })
 	end,
 }
