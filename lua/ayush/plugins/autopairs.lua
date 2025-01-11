ba0e47775a07bff5903e178df3066413853180d0
ayushM <ayushmehrotra099@gmail.com>
2025-01-11 22:59:50 +0530

    added tmux

--- lua/ayush/plugins/autopairs.lua
+++ lua/ayush/plugins/autopairs.lua
@@ -1,31 +1,31 @@
 return {
-  "windwp/nvim-autopairs",
-  event = { "InsertEnter" },
-  dependencies = {
-    "hrsh7th/nvim-cmp",
-  },
-  config = function()
-    -- import nvim-autopairs
-    local autopairs = require("nvim-autopairs")
+	"windwp/nvim-autopairs",
+	event = { "InsertEnter" },
+	dependencies = {
+		"hrsh7th/nvim-cmp", -- nvim-cmp for autocompletion
+	},
+	config = function()
+		-- Import nvim-autopairs
+		local autopairs = require("nvim-autopairs")
 
-    -- configure autopairs
-    autopairs.setup({
-      check_ts = true, -- enable treesitter
-      ts_config = {
-        lua = { "string" }, -- don't add pairs in lua string treesitter nodes
-        javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
-        java = false, -- don't check treesitter on java
-      },
-    })
+		-- Configure nvim-autopairs
+		autopairs.setup({
+			check_ts = true, -- Enable treesitter support
+			ts_config = {
+				lua = { "string" }, -- Don't add pairs in Lua string nodes
+				javascript = { "template_string", "jsx" }, -- Handle JSX and template strings in JS
+				typescript = { "template_string", "jsx" }, -- Handle JSX and template strings in TS
+				java = false, -- Don't check treesitter on Java
+			},
+		})
 
-    -- import nvim-autopairs completion functionality
-    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+		-- Import nvim-autopairs completion functionality
+		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 
-    -- import nvim-cmp plugin (completions plugin)
-    local cmp = require("cmp")
+		-- Import nvim-cmp (completions plugin)
+		local cmp = require("cmp")
 
-    -- make autopairs and completion work together
-    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-  end,
+		-- Make autopairs and completion work together
+		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+	end,
 }
-
