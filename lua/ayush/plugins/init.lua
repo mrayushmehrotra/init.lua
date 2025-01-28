ecaa6ee03e8a2fcc1da92a0657c0d483fc56c88d
ayushM <ayushmehrotra099@gmail.com>
2025-01-28 17:27:34 +0530

    fixed init.lua

--- lua/ayush/plugins/init.lua
+++ lua/ayush/plugins/init.lua
@@ -4,20 +4,20 @@ vim.g.have_nerd_font = vim.g.have_nerd_font or true -- Set this to 'false' if yo
 -- Return the configuration table
 return {
 	-- List of plugins
-	plugins = {
-		"nvim-lua/plenary.nvim", -- lua functions that many plugins use
-		"nvim-telescope/telescope-ui-select.nvim",
-		"christoomey/vim-tmux-navigator", -- tmux & split window navigation
-		"tpope/vim-sleuth",
-		"j-hui/fidget.nvim",
-		{
-			"stevearc/dressing.nvim",
-			event = "VeryLazy",
-		},
+	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
+	"nvim-telescope/telescope-ui-select.nvim",
+	"christoomey/vim-tmux-navigator", -- tmux & split window navigation
+	"tpope/vim-sleuth",
+	"j-hui/fidget.nvim",
+	{
+		"stevearc/dressing.nvim",
+		event = "VeryLazy",
 	},
 
 	-- UI configuration for LazyVim
 	ui = {
+		-- If you are using a Nerd Font, set icons to an empty table to use the default LazyVim icons
+		-- Otherwise, define your own Unicode icons table
 		icons = vim.g.have_nerd_font and {} or {
 			cmd = "⌘",
 			config = "🛠",
